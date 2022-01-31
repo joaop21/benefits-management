4e5ee2f10e73d05fea619e2c89f91338eb3beaf3
joaop21 <jpsilva9898@gmail.com>
2022-01-31 12:46:32 +0000

    Change header location and add accounts facade

--- backend/lib/benefits_web/controllers/user_controller.ex
+++ backend/lib/benefits_web/controllers/user_controller.ex
@@ -20,7 +20,7 @@ defmodule BenefitsWeb.UserController do
     with {:ok, %User{} = user} <- Accounts.create_user(user_params) do
       conn
       |> put_status(:created)
-      |> put_resp_header("location", Routes.user_path(conn, :show, user))
+      |> put_resp_header("location", Routes.user_path(conn, :show, user.user_id))
       |> render("show.json", user: user)
     end
   end

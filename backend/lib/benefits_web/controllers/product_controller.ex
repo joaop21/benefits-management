5bd41efe1d35616de3234a5f90d387fa0c0fecae
joaop21 <jpsilva9898@gmail.com>
2022-01-31 15:22:46 +0000

    Add product controller, view and route in router

---
+++ backend/lib/benefits_web/controllers/product_controller.ex
@@ -0,0 +1,11 @@
+defmodule BenefitsWeb.ProductController do
+  use BenefitsWeb, :controller
+
+  alias Benefits.Perks
+  alias Benefits.Perks.Product
+
+  def index(conn, _params) do
+    products = Perks.list_products()
+    render(conn, "index.json", products: products)
+  end
+end

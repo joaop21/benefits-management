3619604236b17088d60e39c28578aa90f919d3ed
joaop21 <jpsilva9898@gmail.com>
2022-01-31 23:49:15 +0000

    Update Accounts context facade

--- backend/lib/benefits/accounts.ex
+++ backend/lib/benefits/accounts.ex
@@ -41,35 +41,4 @@ defmodule Benefits.Accounts do
     |> User.changeset(attrs)
     |> Repo.insert()
   end
-
-  @doc """
-  Updates a user.
-
-  ## Examples
-
-      iex> update_user(user, %{field: new_value})
-      {:ok, %User{}}
-
-      iex> update_user(user, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_user(%User{} = user, attrs) do
-    user
-    |> User.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking user changes.
-
-  ## Examples
-
-      iex> change_user(user)
-      %Ecto.Changeset{data: %User{}}
-
-  """
-  def change_user(%User{} = user, attrs \\ %{}) do
-    User.changeset(user, attrs)
-  end
 end

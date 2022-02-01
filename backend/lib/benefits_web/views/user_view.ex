e18374f52911144b0c08e06f3121d93c7fc856a7
joaop21 <jpsilva9898@gmail.com>
2022-02-01 16:57:31 +0000

    Update User view

--- backend/lib/benefits_web/views/user_view.ex
+++ backend/lib/benefits_web/views/user_view.ex
@@ -1,12 +1,7 @@
 defmodule BenefitsWeb.UserView do
   use BenefitsWeb, :view
-  alias BenefitsWeb.UserView
 
   def render("show.json", %{user: user}) do
-    render_one(user, UserView, "user.json")
-  end
-
-  def render("user.json", %{user: user}) do
     %{
       user: %{
         user_id: user.user_id,
